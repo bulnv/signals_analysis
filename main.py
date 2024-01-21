86f996a03f82778367cc71dce82a9ec90a21ea28
Nikolai Bulashev <nvbulashev@gmail.com>
2024-01-21 21:09:22 +0100

    parser

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
- 
+import re
+
 # Opening JSON file
 f = open('messages_filtered.json')
  
@@ -14,13 +15,31 @@ def findpair(text):
         if '/' in word:
             if word.split('/')[0].isupper() and word.split('/')[1].isupper():
                 return word.split('/')
+            
 
+def find_entrypoint(text):
+    result = "x"
+    if "Entry" in text:
+        for item in text.split("\n"):
+            if "Entry" in item or "Buy" in item:
+                if ":" in item:
+                    entry_text = item.split(": ")[1]
+                elif " - " in item:
+                    entry_text = item.split(" - ")[1]
+                else:
+                    entry_text = re.sub("[^0-9\-]", "", item).split('-')
+                try:                    
+                    result = [float(re.sub("[^0-9]", "", x)) for x in entry_text]
+                except ValueError:
+                    print(text)
+                return result
 # Iterating through the json
 # list
 for message in data:
     for item in message["text_entities"]: 
         if item["type"] == "plain":
             print(findpair(item["text"]))
+            print(find_entrypoint((item["text"])))
 
  
 # Closing file
